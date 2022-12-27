48e223373b50d6311cb9a6d0466ae4ca230b61ff
initdc <initd@outlook.com>
2022-12-27 03:46:31 +0000

    impr: add private _lang_style_cmd conv func
    Add func document comment

--- lib/libexec.rb
+++ lib/libexec.rb
@@ -4,8 +4,5 @@ require_relative "libexec/version"
 require_relative "libexec/exec"
 
 module Libexec
-  class Error < StandardError; end
-  # Your code goes here...
-
   extend Libexec::Exec
 end
--- lib/libexec/exec.rb
+++ lib/libexec/exec.rb
@@ -2,44 +2,36 @@
 
 module Libexec
   module Exec
-    def run(cmd, *argv, env: {}, mode: "r", **opts)
-      unless argv.empty?
-        cmd = [cmd]
-        argv.each do |arg|
-          # https://docs.ruby-lang.org/en/master/Kernel.html#method-i-class
-          if arg.instance_of?(Array)
-            cmd.concat(arg)
-          elsif arg.instance_of?(String)
-            cmd.push(arg)
-          end
-        end
-      end
+    # wrapper for Ruby IO.popen
+    # {https://docs.ruby-lang.org/en/master/IO.html#method-c-popen link} <br>
+    # cmd first grammar <br>
+    # conv argv by _ruby_style_cmd
+    #
+    # @return [nil]
+    def run(*argv, env: {}, mode: "r", **opts)
+      cmd = _ruby_style_cmd(argv)
 
-      # https://docs.ruby-lang.org/en/master/IO.html#method-c-popen
       IO.popen(env, cmd, mode, opts) do |pipe|
         print pipe.gets until pipe.eof?
       end
       nil
     end
 
-    def each_line(cmd, *argv, env: {}, mode: "r", **opts, &block)
-      unless argv.empty?
-        cmd = [cmd]
-        argv.each do |arg|
-          if arg.instance_of?(Array)
-            cmd.concat(arg)
-          elsif arg.instance_of?(String)
-            cmd.push(arg)
-          end
-        end
-      end
+    # Run code block in IO.popen <br>
+    # inspired from Ruby IO.each_line
+    # {https://docs.ruby-lang.org/en/master/IO.html#method-i-each_line link}
+    def each_line(*argv, env: {}, mode: "r", **opts, &block)
+      cmd = _ruby_style_cmd(argv)
 
-      # https://docs.ruby-lang.org/en/master/IO.html#method-i-each_line
       IO.popen(env, cmd, mode, opts) do |pipe|
         pipe.each_line(&block)
       end
     end
 
+    # wrapper for `ls -1` which will be often used
+    #
+    # @param [String] dir
+    # @return [Array] arr
     def by_ls_1(dir)
       arr = []
       each_line("ls -1 #{dir}") do |line|
@@ -48,15 +40,21 @@ module Libexec
       arr
     end
 
+    # Operate each line result by yourself in `ls -1`
+    #
+    # @param [String] dir
     def each_ls_1(dir, &block)
       each_line("ls -1 #{dir}", &block)
     end
 
+    # wrapper for Process.spawn
+    # {https://docs.ruby-lang.org/en/master/Process.html#method-c-last_status link}
+    #
+    # @return [Integer]
     def code(cmd, *opt, **args)
       catch_error = !opt.empty? || args[:catch_error] || false
       code = opt[0] || args[:code] || 1
 
-      # https://docs.ruby-lang.org/en/master/Process.html#method-c-last_status
       Process.spawn(cmd)
       Process.wait
 
@@ -71,23 +69,68 @@ module Libexec
       127
     end
 
-    def output(cmd, *argv)
-      unless argv.empty?
+    # wrapper for Kernel.`command`
+    # {https://docs.ruby-lang.org/en/master/Kernel.html#method-i-60 link}
+    #
+    # @note Only recommend to use when get single line output
+    # @return [String] output.chomp
+    def output(*argv)
+      cmd = _js_style_cmd(argv)
+
+      output = `#{cmd}`
+      output.chomp
+    end
+
+    private
+
+    # cmd must be unix command String <br>
+    # no " ", not Array <br>
+    # not support cmd with opt like `ls -1`
+    #
+    # @param [Array] argv
+    # @return [String | Array]
+    #
+    # @since 0.2.1
+    def _ruby_style_cmd(argv)
+      cmd, *args = argv
+
+      unless args.empty?
         arr = [cmd]
-        argv.each do |arg|
+        args.each do |arg|
+          # https://docs.ruby-lang.org/en/master/Kernel.html#method-i-class
           if arg.instance_of?(Array)
-            arr.push("'#{arg.join(" ")}'")
+            arr.concat(arg)
           elsif arg.instance_of?(String)
             arr.push(arg)
           end
         end
+        cmd = arr
+      end
+      cmd
+    end
 
+    # in JS style, all arg connect with " " <br>
+    # so cmd like `ls -1` is OK
+    #
+    # @param [Array] argv
+    # @return [String | Array]
+    #
+    # @since 0.2.1
+    def _js_style_cmd(argv)
+      cmd, *args = argv
+
+      unless args.empty?
+        arr = [cmd]
+        args.each do |arg|
+          if arg.instance_of?(Array)
+            arr.push(%('#{arg.join(" ")}'))
+          elsif arg.instance_of?(String)
+            arr.push(arg)
+          end
+        end
         cmd = arr.join(" ")
       end
-
-      # https://docs.ruby-lang.org/en/master/Kernel.html#method-i-60
-      output = `#{cmd}`
-      output.chomp
+      cmd
     end
   end
 end
